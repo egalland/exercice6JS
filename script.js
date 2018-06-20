8f1d0b51f204c4dea5ed327aa529d275a978e5d1
egalland <emmanuel.galland117@gmail.com>
2018-06-20 18:25:09 +0200

    Exercice 6

---
+++ script.js
@@ -0,0 +1,15 @@
+// FONCTION CALCUL
+function calcul(){
+// DECLARATION ET RECUPERATION DES VARIABLES
+var firstNumber = document.getElementById('firstNumber').value;
+var secondNumber = document.getElementById('secondNumber').value;
+// VERIFICATION DES VARIABLES
+ if(/^[0-9.]+$/.test(firstNumber && secondNumber)){
+    // VERIFICATION OK -> MODULO
+    var result = firstNumber % secondNumber;
+    alert(result);
+  }else{
+    // ECHEC
+      alert('Ce n\'est pas un nombre');
+    }
+}
